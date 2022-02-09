e01c4513e0455ab19d20c293fd9ccf7cecb8a895
AlexAshley <aa96sk@gmial.com>
2022-02-09 21:59:24 +0000

    setup tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,14 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
-</template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<div id="background" class="h-screen w-screen -z-50 absolute bg-gray-500"></div>
+
+  <div class="w-screen h-screen flex items-center justify-center">
+    <h2 class="text-9xl uppercase shadow-2xl shadow-amber-800 font-bold font-sans decoration-8 border-2 rounded-2xl border-l-green-300 border-t-purple-400 border-r-red-500 border-b-cyan-400 decoration-black shadow-2xl px-3 pb-2.5 tracking-widest text-lime-300">t.e.s.t</h2>
+  </div>
+
+</template>
